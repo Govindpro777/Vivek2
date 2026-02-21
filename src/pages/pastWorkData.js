60bf54f89411e3553eec997a2e7f40f7213dd4b7
govindweb777 <prajapatigovind866@gmail.com>
2026-02-21 21:05:46 +0530

    fixxxxxxx

--- src/pages/pastWorkData.js
+++ src/pages/pastWorkData.js
@@ -87,177 +87,171 @@
 //   },
 // ]
 
+// Day 1 imports
+import day1_01 from "../../src/assets/Day1/project1.jpeg";
+import day1_02 from "../../src/assets/Day1/1.jpeg";
+import day1_03 from "../../src/assets/Day1/2.jpeg";
+import day1_04 from "../../src/assets/Day1/3.jpeg";
+import day1_05 from "../../src/assets/Day1/4.jpeg";
+import day1_06 from "../../src/assets/Day1/5.jpeg";
+import day1_07 from "../../src/assets/Day1/6.jpeg";
+import day1_08 from "../../src/assets/Day1/7.jpeg";
+import day1_09 from "../../src/assets/Day1/8.jpeg";
+import day1_10 from "../../src/assets/Day1/9.jpeg";
+import day1_11 from "../../src/assets/Day1/10.jpeg";
+import day1_12 from "../../src/assets/Day1/11.jpeg";
+import day1_13 from "../../src/assets/Day1/12.jpeg";
+import day1_14 from "../../src/assets/Day1/13.jpeg";
+import day1_15 from "../../src/assets/Day1/14.jpeg";
+import day1_16 from "../../src/assets/Day1/15.jpeg";
+import day1_17 from "../../src/assets/Day1/16.jpeg";
+import day1_18 from "../../src/assets/Day1/17.jpeg";
+import day1_19 from "../../src/assets/Day1/18.jpeg";
+import day1_20 from "../../src/assets/Day1/19.jpeg";
+import day1_21 from "../../src/assets/Day1/20.jpeg";
+import day1_22 from "../../src/assets/Day1/21.jpeg";
+import day1_23 from "../../src/assets/Day1/22.jpeg";
+import day1_24 from "../../src/assets/Day1/23.jpeg";
+import day1_25 from "../../src/assets/Day1/24.jpeg";
 
-// Day 1 imports (29 images)
-import day1_01 from "@/assets/Day1/project1.jpeg";
-import day1_02 from "@/assets/Day1/1.jpeg";
-import day1_03 from "@/assets/Day1/2.jpeg";
-import day1_04 from "@/assets/Day1/3.jpeg";
-import day1_05 from "@/assets/Day1/4.jpeg";
-import day1_06 from "@/assets/Day1/5.jpeg";
-import day1_07 from "@/assets/Day1/6.jpeg";
-import day1_08 from "@/assets/Day1/7.jpeg";
-import day1_09 from "@/assets/Day1/8.jpeg";
-import day1_10 from "@/assets/Day1/9.jpeg";
-import day1_11 from "@/assets/Day1/10.jpeg";
-import day1_12 from "@/assets/Day1/11.jpeg";
-import day1_13 from "@/assets/Day1/12.jpeg";
-import day1_14 from "@/assets/Day1/13.jpeg";
-import day1_15 from "@/assets/Day1/14.jpeg";
-import day1_16 from "@/assets/Day1/15.jpeg";
-import day1_17 from "@/assets/Day1/16.jpeg";
-import day1_18 from "@/assets/Day1/17.jpeg";
-import day1_19 from "@/assets/Day1/18.jpeg";
-import day1_20 from "@/assets/Day1/19.jpeg";
-import day1_21 from "@/assets/Day1/20.jpeg";
-import day1_22 from "@/assets/Day1/21.jpeg";
-import day1_23 from "@/assets/Day1/22.jpeg";
-import day1_24 from "@/assets/Day1/23.jpeg";
-import day1_25 from "@/assets/Day1/24.jpeg";
+// Day 2 imports
+import day2_01 from "../../src/assets/Day2/project2.jpeg";
+import day2_02 from "../../src/assets/Day2/1.jpeg";
+import day2_03 from "../../src/assets/Day2/2.jpeg";
+import day2_04 from "../../src/assets/Day2/3.jpeg";
+import day2_05 from "../../src/assets/Day2/4.jpeg";
+import day2_06 from "../../src/assets/Day2/5.jpeg";
+import day2_07 from "../../src/assets/Day2/6.jpeg";
+import day2_08 from "../../src/assets/Day2/7.jpeg";
+import day2_09 from "../../src/assets/Day2/8.jpeg";
+import day2_10 from "../../src/assets/Day2/9.jpeg";
+import day2_11 from "../../src/assets/Day2/10.jpeg";
 
+// Day 3 imports
+import day3_01 from "../../src/assets/Day3/project3.jpeg";
+import day3_02 from "../../src/assets/Day3/1.jpeg";
+import day3_03 from "../../src/assets/Day3/2.jpeg";
+import day3_04 from "../../src/assets/Day3/3.jpeg";
+import day3_05 from "../../src/assets/Day3/4.jpeg";
+import day3_06 from "../../src/assets/Day3/5.jpeg";
+import day3_07 from "../../src/assets/Day3/6.jpeg";
+import day3_08 from "../../src/assets/Day3/7.jpeg";
+import day3_09 from "../../src/assets/Day3/8.jpeg";
+import day3_10 from "../../src/assets/Day3/9.jpeg";
+import day3_11 from "../../src/assets/Day3/10.jpeg";
+import day3_12 from "../../src/assets/Day3/11.jpeg";
+import day3_13 from "../../src/assets/Day3/12.jpeg";
+import day3_14 from "../../src/assets/Day3/13.jpeg";
+import day3_15 from "../../src/assets/Day3/14.jpeg";
+import day3_16 from "../../src/assets/Day3/15.jpeg";
+import day3_17 from "../../src/assets/Day3/16.jpeg";
+import day3_18 from "../../src/assets/Day3/17.jpeg";
+import day3_19 from "../../src/assets/Day3/18.jpeg";
+import day3_20 from "../../src/assets/Day3/19.jpeg";
+import day3_21 from "../../src/assets/Day3/20.jpeg";
+import day3_22 from "../../src/assets/Day3/21.jpeg";
+import day3_23 from "../../src/assets/Day3/22.jpeg";
 
-// Day 2 imports (29 images)
-import day2_01 from "@/assets/Day2/project2.jpeg";
-import day2_02 from "@/assets/Day2/1.jpeg";
-import day2_03 from "@/assets/Day2/2.jpeg";
-import day2_04 from "@/assets/Day2/3.jpeg";
-import day2_05 from "@/assets/Day2/4.jpeg";
-import day2_06 from "@/assets/Day2/5.jpeg";
-import day2_07 from "@/assets/Day2/6.jpeg";
-import day2_08 from "@/assets/Day2/7.jpeg";
-import day2_09 from "@/assets/Day2/8.jpeg";
-import day2_10 from "@/assets/Day2/9.jpeg";
-import day2_11 from "@/assets/Day2/10.jpeg";
+// Day 4 imports
+import day4_01 from "../../src/assets/Day4/project4.jpeg";
+import day4_02 from "../../src/assets/Day4/1.jpeg";
+import day4_03 from "../../src/assets/Day4/2.jpeg";
+import day4_04 from "../../src/assets/Day4/3.jpeg";
+import day4_05 from "../../src/assets/Day4/4.jpeg";
+import day4_06 from "../../src/assets/Day4/5.jpeg";
+import day4_07 from "../../src/assets/Day4/6.jpeg";
+import day4_08 from "../../src/assets/Day4/7.jpeg";
+import day4_09 from "../../src/assets/Day4/8.jpeg";
+import day4_10 from "../../src/assets/Day4/9.jpeg";
+import day4_11 from "../../src/assets/Day4/10.jpeg";
+import day4_12 from "../../src/assets/Day4/11.jpeg";
+import day4_13 from "../../src/assets/Day4/12.jpeg";
+import day4_14 from "../../src/assets/Day4/13.jpeg";
+import day4_15 from "../../src/assets/Day4/14.jpeg";
+import day4_16 from "../../src/assets/Day4/15.jpeg";
+import day4_17 from "../../src/assets/Day4/16.jpeg";
+import day4_18 from "../../src/assets/Day4/17.jpeg";
+import day4_19 from "../../src/assets/Day4/18.jpeg";
+import day4_20 from "../../src/assets/Day4/19.jpeg";
+import day4_21 from "../../src/assets/Day4/20.jpeg";
+import day4_22 from "../../src/assets/Day4/21.jpeg";
+import day4_23 from "../../src/assets/Day4/22.jpeg";
+import day4_24 from "../../src/assets/Day4/23.jpeg";
+import day4_25 from "../../src/assets/Day4/24.jpeg";
+import day4_26 from "../../src/assets/Day4/25.jpeg";
+import day4_27 from "../../src/assets/Day4/26.jpeg";
+import day4_28 from "../../src/assets/Day4/27.jpeg";
+import day4_29 from "../../src/assets/Day4/28.jpeg";
 
+// Day 5 imports
+import day5_01 from "../../src/assets/Day5/project5.jpeg";
+import day5_02 from "../../src/assets/Day5/1.jpeg";
+import day5_03 from "../../src/assets/Day5/2.jpeg";
+import day5_04 from "../../src/assets/Day5/3.jpeg";
+import day5_05 from "../../src/assets/Day5/4.jpeg";
+import day5_06 from "../../src/assets/Day5/5.jpeg";
+import day5_07 from "../../src/assets/Day5/6.jpeg";
+import day5_08 from "../../src/assets/Day5/7.jpeg";
+import day5_09 from "../../src/assets/Day5/8.jpeg";
+import day5_10 from "../../src/assets/Day5/9.jpeg";
+import day5_11 from "../../src/assets/Day5/10.jpeg";
+import day5_12 from "../../src/assets/Day5/11.jpeg";
+import day5_13 from "../../src/assets/Day5/12.jpeg";
+import day5_14 from "../../src/assets/Day5/13.jpeg";
+import day5_15 from "../../src/assets/Day5/14.jpeg";
+import day5_16 from "../../src/assets/Day5/15.jpeg";
+import day5_17 from "../../src/assets/Day5/16.jpeg";
+import day5_18 from "../../src/assets/Day5/17.jpeg";
+import day5_19 from "../../src/assets/Day5/18.jpeg";
+import day5_20 from "../../src/assets/Day5/19.jpeg";
+import day5_21 from "../../src/assets/Day5/20.jpeg";
+import day5_22 from "../../src/assets/Day5/21.jpeg";
 
-// Day 3 imports (29 images)
-import day3_01 from "@/assets/Day3/project3.jpeg";
-import day3_02 from "@/assets/Day3/1.jpeg";
-import day3_03 from "@/assets/Day3/2.jpeg";
-import day3_04 from "@/assets/Day3/3.jpeg";
-import day3_05 from "@/assets/Day3/4.jpeg";
-import day3_06 from "@/assets/Day3/5.jpeg";
-import day3_07 from "@/assets/Day3/6.jpeg";
-import day3_08 from "@/assets/Day3/7.jpeg";
-import day3_09 from "@/assets/Day3/8.jpeg";
-import day3_10 from "@/assets/Day3/9.jpeg";
-import day3_11 from "@/assets/Day3/10.jpeg";
-import day3_12 from "@/assets/Day3/11.jpeg";
-import day3_13 from "@/assets/Day3/12.jpeg";
-import day3_14 from "@/assets/Day3/13.jpeg";
-import day3_15 from "@/assets/Day3/14.jpeg";
-import day3_16 from "@/assets/Day3/15.jpeg";
-import day3_17 from "@/assets/Day3/16.jpeg";
-import day3_18 from "@/assets/Day3/17.jpeg";
-import day3_19 from "@/assets/Day3/18.jpeg";
-import day3_20 from "@/assets/Day3/19.jpeg";
-import day3_21 from "@/assets/Day3/20.jpeg";
-import day3_22 from "@/assets/Day3/21.jpeg";
-import day3_23 from "@/assets/Day3/22.jpeg";
+// Day 6 imports
+import day6_01 from "../../src/assets/Day6/project6.jpeg";
+import day6_02 from "../../src/assets/Day6/1.jpeg";
+import day6_03 from "../../src/assets/Day6/2.jpeg";
+import day6_04 from "../../src/assets/Day6/3.jpeg";
+import day6_05 from "../../src/assets/Day6/4.jpeg";
+import day6_06 from "../../src/assets/Day6/5.jpeg";
+import day6_07 from "../../src/assets/Day6/6.jpeg";
+import day6_08 from "../../src/assets/Day6/7.jpeg";
+import day6_09 from "../../src/assets/Day6/8.jpeg";
+import day6_10 from "../../src/assets/Day6/9.jpeg";
+import day6_11 from "../../src/assets/Day6/10.jpeg";
+import day6_12 from "../../src/assets/Day6/11.jpeg";
+import day6_13 from "../../src/assets/Day6/12.jpeg";
+import day6_14 from "../../src/assets/Day6/13.jpeg";
+import day6_15 from "../../src/assets/Day6/14.jpeg";
+import day6_16 from "../../src/assets/Day6/15.jpeg";
+import day6_17 from "../../src/assets/Day6/16.jpeg";
 
-// Day 4 imports (29 images)
-import day4_01 from "@/assets/Day4/project4.jpeg";
-import day4_02 from "@/assets/Day4/1.jpeg";
-import day4_03 from "@/assets/Day4/2.jpeg";
-import day4_04 from "@/assets/Day4/3.jpeg";
-import day4_05 from "@/assets/Day4/4.jpeg";
-import day4_06 from "@/assets/Day4/5.jpeg";
-import day4_07 from "@/assets/Day4/6.jpeg";
-import day4_08 from "@/assets/Day4/7.jpeg";
-import day4_09 from "@/assets/Day4/8.jpeg";
-import day4_10 from "@/assets/Day4/9.jpeg";
-import day4_11 from "@/assets/Day4/10.jpeg";
-import day4_12 from "@/assets/Day4/11.jpeg";
-import day4_13 from "@/assets/Day4/12.jpeg";
-import day4_14 from "@/assets/Day4/13.jpeg";
-import day4_15 from "@/assets/Day4/14.jpeg";
-import day4_16 from "@/assets/Day4/15.jpeg";
-import day4_17 from "@/assets/Day4/16.jpeg";
-import day4_18 from "@/assets/Day4/17.jpeg";
-import day4_19 from "@/assets/Day4/18.jpeg";
-import day4_20 from "@/assets/Day4/19.jpeg";
-import day4_21 from "@/assets/Day4/20.jpeg";
-import day4_22 from "@/assets/Day4/21.jpeg";
-import day4_23 from "@/assets/Day4/22.jpeg";
-import day4_24 from "@/assets/Day4/23.jpeg";
-import day4_25 from "@/assets/Day4/24.jpeg";
-import day4_26 from "@/assets/Day4/25.jpeg";
-import day4_27 from "@/assets/Day4/26.jpeg";
-import day4_28 from "@/assets/Day4/27.jpeg";
-import day4_29 from "@/assets/Day4/28.jpeg";
-
-// Day 5 imports (29 images)
-import day5_01 from "@/assets/Day5/project5.jpeg";
-import day5_02 from "@/assets/Day5/1.jpeg";
-import day5_03 from "@/assets/Day5/2.jpeg";
-import day5_04 from "@/assets/Day5/3.jpeg";
-import day5_05 from "@/assets/Day5/4.jpeg";
-import day5_06 from "@/assets/Day5/5.jpeg";
-import day5_07 from "@/assets/Day5/6.jpeg";
-import day5_08 from "@/assets/Day5/7.jpeg";
-import day5_09 from "@/assets/Day5/8.jpeg";
-import day5_10 from "@/assets/Day5/9.jpeg";
-import day5_11 from "@/assets/Day5/10.jpeg";
-import day5_12 from "@/assets/Day5/11.jpeg";
-import day5_13 from "@/assets/Day5/12.jpeg";
-import day5_14 from "@/assets/Day5/13.jpeg";
-import day5_15 from "@/assets/Day5/14.jpeg";
-import day5_16 from "@/assets/Day5/15.jpeg";
-import day5_17 from "@/assets/Day5/16.jpeg";
-import day5_18 from "@/assets/Day5/17.jpeg";
-import day5_19 from "@/assets/Day5/18.jpeg";
-import day5_20 from "@/assets/Day5/19.jpeg";
-import day5_21 from "@/assets/Day5/20.jpeg";
-import day5_22 from "@/assets/Day5/21.jpeg";
-
-
-// Day 6 imports (29 images)
-import day6_01 from "@/assets/Day6/project6.jpeg";
-import day6_02 from "@/assets/Day6/1.jpeg";
-import day6_03 from "@/assets/Day6/2.jpeg";
-import day6_04 from "@/assets/Day6/3.jpeg";
-import day6_05 from "@/assets/Day6/4.jpeg";
-import day6_06 from "@/assets/Day6/5.jpeg";
-import day6_07 from "@/assets/Day6/6.jpeg";
-import day6_08 from "@/assets/Day6/7.jpeg";
-import day6_09 from "@/assets/Day6/8.jpeg";
-import day6_10 from "@/assets/Day6/9.jpeg";
-import day6_11 from "@/assets/Day6/10.jpeg";
-import day6_12 from "@/assets/Day6/11.jpeg";
-import day6_13 from "@/assets/Day6/12.jpeg";
-import day6_14 from "@/assets/Day6/13.jpeg";
-import day6_15 from "@/assets/Day6/14.jpeg";
-import day6_16 from "@/assets/Day6/15.jpeg";
-import day6_17 from "@/assets/Day6/16.jpeg";
-
-
-// Day 7 imports (29 images)
-import day7_01 from "@/assets/Day7/project7.jpeg";
-import day7_02 from "@/assets/Day7/1.jpeg";
-import day7_03 from "@/assets/Day7/2.jpeg";
-import day7_04 from "@/assets/Day7/3.jpeg";
-import day7_05 from "@/assets/Day7/4.jpeg";
-import day7_06 from "@/assets/Day7/5.jpeg";
-import day7_07 from "@/assets/Day7/6.jpeg";
-import day7_08 from "@/assets/Day7/7.jpeg";
-import day7_09 from "@/assets/Day7/8.jpeg";
-import day7_10 from "@/assets/Day7/9.jpeg";
-import day7_11 from "@/assets/Day7/10.jpeg";
-import day7_12 from "@/assets/Day7/11.jpeg";
-import day7_13 from "@/assets/Day7/12.jpeg";
-import day7_14 from "@/assets/Day7/13.jpeg";
-import day7_15 from "@/assets/Day7/14.jpeg";
-import day7_16 from "@/assets/Day7/15.jpeg";
-import day7_17 from "@/assets/Day7/16.jpeg";
-import day7_18 from "@/assets/Day7/17.jpeg";
-import day7_19 from "@/assets/Day7/18.jpeg";
-import day7_20 from "@/assets/Day7/19.jpeg";
-import day7_21 from "@/assets/Day7/20.jpeg";
-import day7_22 from "@/assets/Day7/21.jpeg";
-
+// Day 7 imports
+import day7_01 from "../../src/assets/Day7/project7.jpeg";
+import day7_02 from "../../src/assets/Day7/1.jpeg";
+import day7_03 from "../../src/assets/Day7/2.jpeg";
+import day7_04 from "../../src/assets/Day7/3.jpeg";
+import day7_05 from "../../src/assets/Day7/4.jpeg";
+import day7_06 from "../../src/assets/Day7/5.jpeg";
+import day7_07 from "../../src/assets/Day7/6.jpeg";
+import day7_08 from "../../src/assets/Day7/7.jpeg";
+import day7_09 from "../../src/assets/Day7/8.jpeg";
+import day7_10 from "../../src/assets/Day7/9.jpeg";
+import day7_11 from "../../src/assets/Day7/10.jpeg";
+import day7_12 from "../../src/assets/Day7/11.jpeg";
+import day7_13 from "../../src/assets/Day7/12.jpeg";
+import day7_14 from "../../src/assets/Day7/13.jpeg";
+import day7_15 from "../../src/assets/Day7/14.jpeg";
+import day7_16 from "../../src/assets/Day7/15.jpeg";
+import day7_17 from "../../src/assets/Day7/16.jpeg";
+import day7_18 from "../../src/assets/Day7/17.jpeg";
+import day7_19 from "../../src/assets/Day7/18.jpeg";
+import day7_20 from "../../src/assets/Day7/19.jpeg";
+import day7_21 from "../../src/assets/Day7/20.jpeg";
+import day7_22 from "../../src/assets/Day7/21.jpeg";
 
 // ──────────────────────────────────────────────
-// Image arrays — exactly 29 items each
+// Image arrays
 // ──────────────────────────────────────────────
 
 const imagesDay1 = [
@@ -279,7 +273,7 @@ const imagesDay3 = [
   day3_06, day3_07, day3_08, day3_09, day3_10,
   day3_11, day3_12, day3_13, day3_14, day3_15,
   day3_16, day3_17, day3_18, day3_19, day3_20,
-  day3_21, day3_22, day3_23, 
+  day3_21, day3_22, day3_23,
 ];
 
 const imagesDay4 = [
@@ -296,14 +290,14 @@ const imagesDay5 = [
   day5_06, day5_07, day5_08, day5_09, day5_10,
   day5_11, day5_12, day5_13, day5_14, day5_15,
   day5_16, day5_17, day5_18, day5_19, day5_20,
-  day5_21, day5_22, 
+  day5_21, day5_22,
 ];
 
 const imagesDay6 = [
   day6_01, day6_02, day6_03, day6_04, day6_05,
   day6_06, day6_07, day6_08, day6_09, day6_10,
   day6_11, day6_12, day6_13, day6_14, day6_15,
-  day6_16, day6_17, 
+  day6_16, day6_17,
 ];
 
 const imagesDay7 = [
